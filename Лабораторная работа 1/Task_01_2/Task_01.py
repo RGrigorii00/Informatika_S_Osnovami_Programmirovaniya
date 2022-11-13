75e44f66a2a58d40529aad6fc0431ec1f86b7768
RGrigorii <118053826+RGrigorii00@users.noreply.github.com>
2022-11-13 21:45:56 +0500

    Add files via upload

---
+++ Лабораторная работа 1/Task_01_2/Task_01.py
@@ -0,0 +1,16 @@
+def Task_01():
+# Description / Interface
+	print("Введите ваше число: ");
+
+# Var's
+	chislo = int(input());
+	result = "";
+
+# Logic
+	if (chislo % 2 != 0):
+		print("Ваше число нечетное");
+	else:
+		print("Ваше число четное");
+
+	import main;
+	main.Start();
